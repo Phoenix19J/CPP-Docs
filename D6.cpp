3822592cc5c47fee9c606d648322b15ee989a1dd
Phoenix19J <paramdalal123@gmail.com>
2023-10-17 16:34:02 +0530

    Added D6.cpp file

---
+++ D6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+
+
+using std::cin;
+using std::cout;
+using std::string;
+
+
+void Cignore_max() {
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');      // Clears input buffer, better than using fflush(stdin) and std::ws in getline()
+}
+
+int main(){
+
+
+
+
+
+
+
+
+
+    return 0;
+}
