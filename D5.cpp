4993dd5588582c8bed50f6c193ba55fbe0f2a385
Phoenix19J <paramdalal123@gmail.com>
2023-10-17 16:34:46 +0530

    Files upto D5.cpp completed

--- D5.cpp
+++ D5.cpp
@@ -11,11 +11,11 @@ using std::string;
 
 
 void Cignore_max() {
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');   // Clears input buffer, better than using fflush(stdin) and std::ws in getline()
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');      // Clears input buffer, better than using fflush(stdin) and std::ws in getline()
 }
 string uppercase_for_loop(string normal_text) {
-    for (int i = 0; i < normal_text.length(); i++) {                 //Two different method same results
-        normal_text[i] = toupper(normal_text[i]);                    // toupper() method via array for loop
+    for (int i = 0; i < normal_text.length(); i++) {                    // Two different method same results
+        normal_text[i] = toupper(normal_text[i]);                       // toupper() method via array for loop
     }
 
     //#transform(name.begin(), name.end(), name.begin(), ::toupper); // transform() method directly ---need <algorithm> header file---
@@ -31,13 +31,19 @@ char ABCarray(char array[], int size_array) {
 }
 int searchArray(int array[], int array_size, int input_num_find) {
     for (int i = 0;i < array_size;i++) {
-        if(array[i] == input_num_find){
+        if (array[i] == input_num_find) {
             return i;
         }
     }
     return -1;
 }
 
+string Printarray(string array[], int size) {
+    for (int i = 0; i < size; i++)
+    {
+        return array[i];
+    }
+}
 
 int main()
 {
@@ -80,18 +86,49 @@ int main()
     cout << "\n\nEnter element to search for: ";
     cin >> inputnum;
     index = searchArray(arr, size, inputnum);
-    if (index!=-1){
-        cout<<"Your number is at index : " <<index<<"\n\n";
-    }else{
-        cout<<"Your number was not found in array !\n\n";
+    if (index != -1) {
+        cout << "Your number is at index : " << index << "\n\n";
     }
-    
+    else {
+        cout << "Your number was not found in array !\n\n";
+    }
+
     std::sort(arr, arr + size);                                         //* Sorting an array --greater<int>()-- descending order ___Basic Sorting___
-    std::sort(arr, arr + size,std::greater<int>());
+    std::sort(arr, arr + size, std::greater<int>());
     for (int i : arr) {
         cout << i << " ";
     }
+    cout << "\n\n";
 
+
+    string foods[10];
+    fill(foods, foods + 6, "pizza");                                   //* Fill function--takes 3 parameters.
+
+    int Strsize = sizeof(foods) / sizeof(foods[0]);                     // Prints only filled items in a array, leaves empty ones.
+    for (int i = 0; !foods[i].empty(); i++)
+    {
+        cout << Printarray(foods, Strsize) << "\n";
+    }
+    cout << "\n\n";
+
+
+    int matrix[][3] = { {1,2,3},                                        //* 2-D Arrays
+                        {4,5,6},
+                        {7,8,9} };
+    int rows = sizeof(matrix) / sizeof(matrix[0]);
+    int columns = sizeof(matrix[0]) / sizeof(matrix[0][0]);
+    cout << rows << " " << columns << "\n\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
+    cout << "Address of matrix is " << &matrix << "\n";                 //* Memory Addresses
 
 
     return 0;
